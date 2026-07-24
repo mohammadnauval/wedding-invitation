de5e01393df75780c6dbba0614fe6fcfd0d93468
mohnauval <mohammad.nauval@iforte.co.id>
2026-07-25 00:19:05 +0700

    Remove top padding to push text higher

--- src/components/wedding/CoverSection.jsx
+++ src/components/wedding/CoverSection.jsx
@@ -24,7 +24,7 @@ function CoverSection({ weddingData, guestName, onOpen }) {
         />
       </div>
 
-      <div className="max-w-[480px] mx-auto w-full h-full flex flex-col justify-start px-8 pt-12 pb-8 relative">
+      <div className="max-w-[480px] mx-auto w-full h-full flex flex-col justify-start px-8 pt-4 pb-8 relative">
         
         {/* Top section - names and title, positioned at top like reference */}
         <div className="text-center">
