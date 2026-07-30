36d788c76afd6311b61924f4415a00dce195b632
mohnauval <mohammad.nauval@iforte.co.id>
2026-07-31 00:38:20 +0700

    Use object-fill to make counting days background fill entire section

--- src/components/wedding/CountdownSection.jsx
+++ src/components/wedding/CountdownSection.jsx
@@ -65,7 +65,7 @@ function CountdownSection({ weddingData }) {
     <section ref={ref} className="relative py-16 overflow-hidden bg-[var(--color-bg)]">
       {/* Background image */}
       <div className="absolute inset-0">
-        <img src="/images/counting_days_background_new.png" alt="" className="w-full h-full object-contain" />
+        <img src="/images/counting_days_background_new.png" alt="" className="w-full h-full object-fill" />
         <div className="absolute inset-0 bg-[var(--color-bg)]/50" />
       </div>
 
