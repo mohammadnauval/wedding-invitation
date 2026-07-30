7d601895f9b28deea994b67ca9b39b13db00426d
mohnauval <mohammad.nauval@iforte.co.id>
2026-07-31 01:51:24 +0700

    Increase countdown section to pb-72 for full background visibility

--- src/components/wedding/CountdownSection.jsx
+++ src/components/wedding/CountdownSection.jsx
@@ -62,7 +62,7 @@ function CountdownSection({ weddingData }) {
   const calendar = getCalendarWeek();
 
   return (
-    <section ref={ref} className="relative pt-12 pb-52 overflow-hidden bg-[var(--color-bg)]">
+    <section ref={ref} className="relative pt-12 pb-72 overflow-hidden bg-[var(--color-bg)]">
       {/* Background image */}
       <div className="absolute inset-0">
         <img src="/images/counting_days_background_7.png" alt="" className="w-full h-full object-cover" />
